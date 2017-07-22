9a6cef97a4427e3eebfe183fb515b82d8f2af570
mcallaghan <max.w.callaghan@gmail.com>
2017-07-22 16:04:42 +0200

    cluster script ready

--- cluster/do_citation_network.py
+++ cluster/do_citation_network.py
@@ -1,5 +1,9 @@
 import numpy as np
+
+from scipy.sparse import csr_matrix, find, tril
+import csv
 import igraph
+
 
 def load_sparse_csr(filename):
     loader = np.load(filename)
@@ -8,4 +12,47 @@ def load_sparse_csr(filename):
 
 m = load_sparse_csr("1457_citations.npz")
 
-print(m)
+#subg = list(range(10000))
+#m = m.tocsr()[subg, :].tocsc()[:, subg]
+
+nodes = m.shape[0]
+
+print(nodes)
+
+with open("1457_nodelabels.txt") as f:
+    c = csv.reader(f,delimiter='\t')
+    ids = [r[1] for r in c]
+
+
+g = igraph.Graph()
+g.add_vertices(nodes)
+
+mat = find(m)
+edge_n = len(mat[0])
+
+g.add_edges(zip(mat[0],mat[1]))
+
+igraph.summary(g)
+
+g.vs["wosid"] = ids
+
+giant = g.clusters().giant().simplify()
+
+igraph.summary(giant)
+
+
+dendogram = giant.community_fastgreedy()
+
+clusters = dendogram.as_clustering()
+
+membership = clusters.membership
+
+with open("output","w") as w:
+    writer = csv.writer(w)
+    for i in range(len(membership)):
+        name = giant.vs["wosid"][i]
+        mem = membership[i]
+        writer.writerow([name,mem])
+
+    
+
