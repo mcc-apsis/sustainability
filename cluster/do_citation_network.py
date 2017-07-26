d676a94930195785cf78f5ea55df0da13e32a11a
mcallaghan <max.w.callaghan@gmail.com>
2017-07-26 16:22:33 +0200

    community graphs

--- cluster/do_citation_network.py
+++ cluster/do_citation_network.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from scipy.sparse import csr_matrix, find, tril
-import csv
+import csv, sys
 import igraph
 
 
@@ -12,7 +12,7 @@ def load_sparse_csr(filename):
 
 m = load_sparse_csr("1457_citations_lutz.npz")
 
-#subg = list(range(10000))
+#subg = list(range(50000))
 #m = m.tocsr()[subg, :].tocsc()[:, subg]
 
 nodes = m.shape[0]
@@ -49,19 +49,44 @@ giant = g.clusters().giant().simplify()
 
 igraph.summary(giant)
 
-igraph.plot(giant,"connected_network.pdf")
+visual_style = {}
 
-igraph.plot(g,"network.pdf")
+visual_style["edge_width"] = 0.001
+visual_style["vertex_size"] = 0.1
+visual_style["edge_color"] = "rgba(1,1,1,0.1)"
+
+igraph.plot(giant,"connected_network.pdf", **visual_style)
+
+#igraph.plot(g,"network.pdf", **visual_style)
 
 
 dendogram = giant.community_fastgreedy()
 
 clusters = dendogram.as_clustering()
 
+cn = 0
+
+visual_style["vertex_label"] = ["" for x in clusters.membership]
+
 membership = clusters.membership
 
+for c in clusters:
+    cn+=1
+    degrees = [giant.degree(x) for x in c]
+    i = degrees.index(max(degrees))
+    visual_style["vertex_label"][i] = membership[i]
+    
+
+
+
+#visual_style["vertex_label"] = membership
+
+#igraph.plot(dendogram,"dend_network.pdf", mark_groups=True, **visual_style)
+
+igraph.plot(clusters,"clust_network.pdf", mark_groups=True, **visual_style)
+
 with open("output","w") as w:
-    writer = csv.writer(w)
+    writer = csv.writer(w,delimiter='\t')
     for i in range(len(membership)):
         name = giant.vs["wosid"][i]
         ti = giant.vs["title"][i]
